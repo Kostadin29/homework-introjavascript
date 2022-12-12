75d980a92919c009c13a66d29a84a4a76da7e42d
Kostadin <117443351+Kostadin29@users.noreply.github.com>
2022-12-12 20:29:09 +0100

    Update app.js

--- exercise 1/app.js
+++ exercise 1/app.js
@@ -4,7 +4,7 @@ let phones = 30;
 
 let tax = 5.9;
 
-let price = phonePrice - tax;
+let price = phonePrice + tax;
 
 let originalPrice = phones * price;
 console.log("Price for all phones is ", originalPrice);
