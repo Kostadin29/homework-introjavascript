8a260f34a12cee12c0bad93f716fff45ba1ad612
Kostadin <117443351+Kostadin29@users.noreply.github.com>
2022-12-12 20:05:43 +0100

    JavaScript - First Homework
    This is the first homework of JavaScript from Kostadin Petkov

---
+++ exercise 1/app.js
@@ -0,0 +1,10 @@
+let phonePrice = 119.95;
+
+let phones = 30;
+
+let tax = 5.9;
+
+let price = phonePrice - tax;
+
+let originalPrice = phones * price;
+console.log("Price for all phones is ", originalPrice);
